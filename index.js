e55bac74baa0c9c37738622d63ffe464f16188fc
cropped33 <bhandaricropped33@gmail.com>
2022-02-04 17:59:30 +0545

    drumset first upload

---
+++ index.js
@@ -0,0 +1,51 @@
+//function to play sounds.
+function hear(keyword) {
+
+    switch (keyword) {
+        case "W":
+            var audio = new Audio("sounds//tom-1.mp3");
+            audio.play();
+            break;
+        case "A":
+            var audio = new Audio("sounds//tom-2.mp3");
+            audio.play();
+            break;
+        case "S":
+            var audio = new Audio("sounds//tom-3.mp3");
+            audio.play();
+            break;
+        case "D":
+            var audio = new Audio("sounds//tom-4.mp3");
+            audio.play();
+            break;
+        case "J":
+            var audio = new Audio("sounds//crash.mp3");
+            audio.play();
+            break;
+        case "K":
+            var audio = new Audio("sounds//kick-bass.mp3");
+            audio.play();
+            break;
+        case "L":
+            var audio = new Audio("sounds//snare.mp3");
+            audio.play();
+            break;
+
+        default: console.log(this.innerText);
+            break;
+    }
+}
+
+
+//for keypress in keyboard
+document.addEventListener("keypress", function (event) {
+    hear((event.key).toUpperCase());
+});
+
+//for clicks on screen
+for (var i = 0; i < document.querySelectorAll(".dm").length; i++) {
+    document.querySelectorAll(".dm")[i].addEventListener("click", function () {
+        hear(this.innerText);
+    });
+}
+
